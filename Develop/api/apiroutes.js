4e7d61164301db56ebb02f7a17749dd200d55895
cjclap <dmxtechinn@gmail.com>
2022-10-31 19:25:15 -0600

    added routes for get and post, working on delete

---
+++ Develop/api/apiroutes.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const noteData = require('../db/db.json');
+const router = require("express").Router();
+const { uuid } = require('uuidv4');
+// GET// GET Route for retrieving all the note
+router.get('/api/notes', (req, res) => {
+    //JSON.parse(noteData)
+
+    res.json(noteData)
+});
+
+
+router.post('/api/notes', (req, res) => {
+    console.log(req.body);
+
+    if (req.body) {
+        const newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuid(),
+
+        };
+        noteData.push(newNote);
+
+
+
+        fs.writeFile('./db/db.json', JSON.stringify(noteData), (err) => {
+            if (err) {
+                console.log(console.error());
+            }
+            console.log(`Note added successfully 🚀`);
+        })
+
+    }
+});
+
+router.delete(`/api/notes/:id`, (req, res) => {
+    const noteId = req.params.id;
+
+    db = noteData.filter((note) => note.id != noteId);
+    writeToFile('./db/db.json', noteData);
+
+    // Respond to the DELETE request
+    res.json(`Item ${noteId} has been deleted 🗑️`);
+});
+
+module.exports = router;
+// fs.readFile('./db/db.json', 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err);
+//     } else {
+
+//         const parsedNotes = JSON.parse(data);
+//         parsedNotes.push(newNote);
+//         fs.writeFile(
+//             './db/db.json',
+//             JSON.stringify(parsedReviews, null, 4),
+//             (writeErr) =>
+//                 writeErr
+//                     ? console.error(writeErr)
+//                     : console.info('Successfully updated reviews!')
+//         );
+//     }
+// });
+
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.use(express.static('public'));
+//
+// // DELETE Route for a specific note
+
+
+
--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,16 @@
 const express = require('express');
-const path = require('path');
-const api = require('./routes/apiRoute.js');
-const express = require('express');
+const app = express();
+const PORT = 3005;
+const htmlroutes = require("./api/htmlroutes");
+const apiroutes = require("./api/apiroutes")
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(apiroutes);
+app.use(htmlroutes);
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
